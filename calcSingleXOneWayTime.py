30b11c1657e563838edd8ae0a59774dbde1c514a
Alex Wang <alexwang2017@u.northwestern.edu>
2014-10-17 01:20:30 -0500

    Commented and Debugged calcSingleXOneWayTime

--- calcSingleXOneWayTime.py
+++ calcSingleXOneWayTime.py
@@ -11,24 +11,31 @@ def calcSingleXOneWayTotalTime(vehicleRouteFileName,trafficLightFileName):
 	trafficLightArray = fileToArray(trafficLightFileName)
 	totalTime = 0
 	numStops = 0
+
+	# iterate through each car in vehicleRouteArray and add up individual times
 	for i in range(0,len(vehicleRouteArray)):
 		j = 0
 		isNS = True
-		print('i' + str(i))
-		print('j' + str(j))
-		while (j<len(trafficLightArray) and 3 + float(vehicleRouteArray[i][0]) > float(trafficLightArray[j][0])):
+		# iterate through each light change and check when vehicle[i] arrives at the intersection
+		while (j<len(trafficLightArray)) and 3 + float(vehicleRouteArray[i][0]) > float(trafficLightArray[j][0])
 			j += 1
 			isNS = not(isNS)
-			print('j' + str(j))
+		#on exiting while loop, j is the index of the light change after vehicle[i] arrives and isNS is the 
+		#direction of the light when it gets there
+		#in the case that there is no light change after vehicle arrives, j = len(trafficLightArray)
 		if (j<len(trafficLightArray)):
-			if ((vehicleRouteArray[i][1][0]=='B' or vehicleRouteArray[i][1][0]=='D') != (not isNS)):
-				totalTime += float(trafficLightArray[j][0]) - float(vehicleRouteArray[i][0]) - 2
+			#if car gets stopped at traffic light
+			if((vehicleRouteArray[i][1][0]=='B' or vehicleRouteArray[i][1][0]=='D') == (isNS)):
+				#add the time the car had to wait: from time it gets to intersection to one second after light change
+				totalTime += (float(trafficLightArray[j][0]) + 1) - (float(vehicleRouteArray[i][0]) + 3)
 				numStops += 1
-			elif (abs(float(vehicleRouteArray[i][0]) + 2.5 - float(trafficLightArray[j][0])) < 0.5):
-				totalTime += 1 + float(trafficLightArray[j][0]) - float(vehicleRouteArray[i][0]) - 3
-				numStops += 1
+			#else if the car arrives at intersection within a second of the light turning green
+			#true if time that the car reaches the intersection is less than one greater than the time of the (j-1)th light change
+			elif (j > 0 and float(vehicleRouteArray[i][0]) + 3 - float(trafficLightArray[j-1][0]) < 1):
+				# add the time between one second after light change and time car reaches intersection
+				totalTime += (1 + float(trafficLightArray[j-1][0])) - (float(vehicleRouteArray[i][0]) + 3)
+				numStop += 1
 		totalTime += 7
-
 
 	print(numStops)
 	return totalTime
